93aa18f52e4489dbecfbd9f3a39ecae27c5d52da
Gaurav Singh <thakurgaurav273@gmail.com>
2026-05-15 00:58:49 +0530

    minor ui addition

--- src/page-components/Issues/index.tsx
+++ src/page-components/Issues/index.tsx
@@ -1,4 +1,6 @@
 import { Text } from "@/components/ui";
+import { SIZES } from "@/lib/constants";
+import { ListFilter, SlidersHorizontal } from "lucide-react";
 const Issues = () => {
   const instantFilters = [
     { name: "Todo", value: "todo" },
@@ -7,8 +9,12 @@ const Issues = () => {
   ];
 
   const actionButtons = [
-    { name: "Create Issue", value: "create" },
-    { name: "Import Issues", value: "import" },
+    { name: "Create Issue", value: "create", icon: <ListFilter size={SIZES.ICON_SIZES.md} /> },
+    {
+      name: "Import Issues",
+      value: "import",
+      icon: <SlidersHorizontal size={SIZES.ICON_SIZES.md} />,
+    },
   ];
   return (
     <div className="flex flex-col gap-2">
@@ -16,7 +22,7 @@ const Issues = () => {
         My Issues
       </Text>
       <div className="flex items-center justify-between">
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 ">
           {instantFilters.map((filter) => (
             <Text
               key={filter.value}
@@ -26,7 +32,19 @@ const Issues = () => {
               {filter.name}
             </Text>
           ))}
-          <div className="flex gap-2"></div>
+        </div>
+        <div className="flex gap-2">
+          {actionButtons.map(
+            (action) =>
+              action.icon && (
+                <div
+                  key={action.value}
+                  className="flex items-center gap-1 rounded-full p-2 text-white cursor-pointer border border-[var(--text-muted)]"
+                >
+                  {action.icon}
+                </div>
+              )
+          )}
         </div>
       </div>
     </div>
